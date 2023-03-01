12396b61d3bd50e97ed1daa3b3988ecc42f2e70e
Kevin Teillier <kt.teillier@gmail.com>
2023-03-01 11:37:06 +0100

    scss home page done

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,11 @@ const Card = ({card}) => {
     console.log(card);
     return (
      <article className='location-card'>
-        <img className='location-card__image'src={card.cover} alt={card.title} />
-        <h3 className='location-card__title'>{card.title}</h3>
+        <a href='#'>
+            <img className='location-card__image'src={card.cover} alt={card.title} />
+            <h3 className='location-card__title'>{card.title}</h3>
+        </a>
+        
      </article>
     );
 };
--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -4,7 +4,6 @@ const HomeBanner = () => {
     return (
         <div className='home-banner'>
             <h1 className='home-banner__title'>Chez vous, partout et ailleurs</h1>
-            <img className='home-banner__image'src="background.png" alt="Paysage en arrière plan" />
         </div>
     );
 };
