bf01021855ac2cf271f6fe1fed820702bcdb1c6b
Kevin Teillier <kt.teillier@gmail.com>
2023-03-01 15:03:32 +0100

    accordion component modifications

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,46 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Accordion = () => {
-    return (
-        <div className='accordion-container'>
-            <div className ='accordion-container__card'>
-                <div className ='accordion-container__card-heading'>
-                    <h2 className='accordion-container__card-title'>Fiabilité</h2>
-                    <span>
-                        <img src="Vector.png" alt="icône flèche vers le bas" />
-                    </span>
-                </div>
-                <p className='accordion-container__card-content'>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
-            </div>
-            <div className ='accordion-container__card'>
-                <div className ='accordion-container__card-heading'>
-                    <h2 className='accordion-container__card-title'>Respect</h2>
-                    <span>
-                        <img src="Vector.png" alt="icône flèche vers le bas" />
-                    </span>
-                </div>
-                <p className='accordion-container__card-content'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-            </div>
-            <div className ='accordion-container__card'>
-                <div className ='accordion-container__card-heading'>
-                    <h2 className='accordion-container__card-title'>Service</h2>
-                    <span>
-                        <img src="Vector.png" alt="icône flèche vers le bas" />
-                    </span>
-                </div>
-                <p className='accordion-container__card-content'>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
-            </div>
-            <div className ='accordion-container__card'>
-                <div className ='accordion-container__card-heading'>
-                    <h2 className='accordion-container__card-title'>Sécurité</h2>
-                    <span>
-                        <img src="Vector.png" alt="icône flèche vers le bas" />
-                    </span>
-                </div>
-                <p className='accordion-container__card-content'>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-            </div>
-        </div>
-    );
+const Accordion = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <li className="accordion-item">
+      <div className="accordion-toggle" onClick={() => setIsActive(!isActive)}>
+        <h3>{title}</h3><span>{isActive ? "-" : "+"}</span>
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </li>
+  );
 };
 
 export default Accordion;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import AboutBackground from "../components/AboutBackground";
 import Accordion from "../components/Accordion";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const About = () => {
+  const [active, setActive] = useState("");
   return (
     <div>
       <Header />
       <AboutBackground />
-      <Accordion />
+      <Accordion title="Fiabilité" active={active} setActive={setActive} content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."/>
+      <Accordion title="Respect" active={active} setActive={setActive} content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+      <Accordion title="Service" active={active} setActive={setActive} content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
+      <Accordion title="Sécurité" active={active} setActive={setActive} content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
       <Footer />
     </div>
   );
