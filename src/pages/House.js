838c1969b36cb9d59d806052e8390312a4a3dfe9
Kevin Teillier <kt.teillier@gmail.com>
2023-03-07 10:51:28 +0100

    get house details with useParams and useEffect/state, without style and slideshow

--- src/pages/House.js
+++ src/pages/House.js
@@ -1,16 +1,44 @@
 import Header from "../components/Header";
+import Accordion from "../components/Accordion";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
+import data from '../data.json';
+import {  useEffect, useState } from "react";
+import Error from "../pages/Error404"
+
+
 
 const House = () => {
+
+    const { id } = useParams();
+    const [item, setItem, active, setActive] = useState();
+
+    useEffect(() => {
+        const houseDetail = data.find((e) => e.id === id);
     
-    let { id } = useParams();
-    console.log( { id});
-  
-   
+        setItem(houseDetail);
+      });
+
+      if (!item) {
+        return <Error />;
+      }
+    
+    const equipmentList = item.equipments.map((e) => <li key={e}>{e}</li>);
+      
     return (
         <div>
             <Header />
+            <h1>{item.title}</h1>
+            <h2>{item.location}</h2>
+            <ul>
+                { item.tags.map((tag) => ( 
+                <li key={tag}>{tag}</li>
+                ))}
+            </ul>
+            <p>{item.host.name}</p>
+            <img src="{item.host.picture}" alt="Hôte" />
+            <Accordion title="Description" active={active} setActive={setActive} content={item.description}/>
+            <Accordion title="Equipements" active={active} setActive={setActive} content={equipmentList}/>
             <Footer />
         </div>
     );
