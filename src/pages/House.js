e7b7aa19cc7f0991ad183212ea51d4e29fe2c1e6
Kevin Teillier <kt.teillier@gmail.com>
2023-03-07 11:01:09 +0100

    classNames added

--- src/pages/House.js
+++ src/pages/House.js
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import Slideshow from "../components/Slideshow"
 import Accordion from "../components/Accordion";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
@@ -26,17 +27,24 @@ const House = () => {
     const equipmentList = item.equipments.map((e) => <li key={e}>{e}</li>);
       
     return (
-        <div>
+        <div className="House-informations">
             <Header />
-            <h1>{item.title}</h1>
-            <h2>{item.location}</h2>
-            <ul>
-                { item.tags.map((tag) => ( 
-                <li key={tag}>{tag}</li>
-                ))}
-            </ul>
-            <p>{item.host.name}</p>
-            <img src="{item.host.picture}" alt="Hôte" />
+            <Slideshow />
+            <div className="House-informations__location">
+                <h1>{item.title}</h1>
+                <h2>{item.location}</h2>
+            </div>
+            <div className="House-informations__tags">
+                <ul>
+                    { item.tags.map((tag) => ( 
+                    <li key={tag}>{tag}</li>
+                    ))}
+                </ul>
+            </div>
+            <div className="House-informations__host">
+                <p className="House-informations__host-name">{item.host.name}</p>
+                <img className="House-informations__host-picture" src={item.host.picture} alt="Hôte" />
+            </div>
             <Accordion title="Description" active={active} setActive={setActive} content={item.description}/>
             <Accordion title="Equipements" active={active} setActive={setActive} content={equipmentList}/>
             <Footer />
