901507eb3dbeeb79763a651b650760f63d219683
Kevin Teillier <kt.teillier@gmail.com>
2023-04-05 11:33:43 +0200

    linear gradient done on cards home page

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,13 +5,13 @@ import ArrowUp from "../assets/arrowUp.png";
 const Accordion = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
   return (
-    <li className="accordion-item">
+    <div className="accordion-item">
       <div className="accordion-toggle" onClick={() => setIsActive(!isActive)}>
         <h3>{title}</h3>
         {isActive ? <img src={ArrowUp} alt="Flèche vers le haut" /> : <img src={ArrowDown} alt="Flèche vers le bas" />}
       </div>
       {isActive && <div className="accordion-content">{content}</div>}
-    </li>
+    </div>
   );
 };
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,10 @@ const Card = ({card}) => {
         return (
             <article className='location-card'>
                <Link to={`/house/${card.id}`}>
-               <img className='location-card__image'src={card.cover} alt={card.title} />
+               <div className='location-card-container'>
+                  <div className='gradient'></div>
+                  <img className='location-card__image'src={card.cover} alt={card.title} />
+               </div>
                <h3 className='location-card__title'>{card.title}</h3>
                </Link>
             </article>
